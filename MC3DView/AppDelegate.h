21e37766f845403824cc2228cef80e572cc4741b
Baglan Dosmagambetov <baglan.dos@gmail.com>
2012-08-07 11:56:42 +0600

    Initial Commit

---
+++ MC3DView/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  MC3DView
+//
+//  Created by Baglan on 8/7/12.
+//  Copyright (c) 2012 MobileCreators. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ MC3DView/ViewController.m
@@ -0,0 +1,34 @@
+//
+//  ViewController.m
+//  MC3DView
+//
+//  Created by Baglan on 8/7/12.
+//  Copyright (c) 2012 MobileCreators. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+@end
